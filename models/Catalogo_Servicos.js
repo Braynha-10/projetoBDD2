7d44db59a2570e920b1da58b9ee822fe0a6c7034
Braynha-10 <bryansscarvalho16@gmail.com>
2024-11-25 22:08:41 -0300

    arrumando model de catalogo servico 2

--- models/Catalogo_Servicos.js
+++ models/Catalogo_Servicos.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-    const Catalogo_Servico = sequelize.define('Catalogo_Servico', {
+    const Catalogo_Servico = sequelize.define('Catalogo_servico', {
         nome: DataTypes.STRING,
         descricao: DataTypes.STRING,
         preco: DataTypes.DECIMAL
